dadef18e20f0bca5639bca805aecfbaefb6671fa
rossanodan <dangelo.rossano.94@gmail.com>
2019-12-04 16:29:55 +0000

    day-2

--- day-2/index.ts
+++ day-2/index.ts
@@ -1,44 +1,42 @@
 var fs = require('fs');
 
 function index3(noun: number = 12, verb: number = 2): number {
-    const read = fs.readFileSync('day-2/input.txt', 'utf-8');
-    const codes = read.split(',');
-    codes[1] = noun; // noun
-    codes[2] = verb; // verb
-    let i = 0;
-    while (codes[i] != '99') {
-        const opcode = parseInt(codes[i]);
-        const firstNumberIndex = parseInt(codes[i + 1]);
-        const secondNumberIndex = parseInt(codes[i + 2]);
-        const storageIndex = parseInt(codes[i + 3]);
-        let result;
-        switch (opcode) {
-            case 1:
-                result = parseInt(codes[firstNumberIndex]) + parseInt(codes[secondNumberIndex]);
-                codes[storageIndex] = result;
-                break;
-            case 2:
-                result = parseInt(codes[firstNumberIndex]) * parseInt(codes[secondNumberIndex]);
-                codes[storageIndex] = result;
-                break;
-        }
-        i += 4;
+  const read = fs.readFileSync('day-2/input.txt', 'utf-8');
+  const codes = read.split(',');
+  codes[1] = noun; // noun
+  codes[2] = verb; // verb
+  let i = 0;
+  while (codes[i] != '99') {
+    const opcode = parseInt(codes[i]);
+    const firstNumberIndex = parseInt(codes[i + 1]);
+    const secondNumberIndex = parseInt(codes[i + 2]);
+    const storageIndex = parseInt(codes[i + 3]);
+    let result;
+    switch (opcode) {
+      case 1:
+        result = parseInt(codes[firstNumberIndex]) + parseInt(codes[secondNumberIndex]);
+        codes[storageIndex] = result;
+        break;
+      case 2:
+        result = parseInt(codes[firstNumberIndex]) * parseInt(codes[secondNumberIndex]);
+        codes[storageIndex] = result;
+        break;
     }
-    return codes[0];
+    i += 4;
+  }
+  return codes[0];
 }
 
 function index4(): number {
-    const read = fs.readFileSync('day-2/input-2.txt', 'utf-8');
-    const codes = read.split(',');
-    for (let noun = 0; noun <= 99; noun++) {
-        for (let verb = 0; verb <= 99; verb++) {
-            const target = index3(noun, verb);
-            if (target === 19690720) {
-                return 100 * noun + verb;
-            }
-        }
+  for (let noun = 0; noun <= 99; noun++) {
+    for (let verb = 0; verb <= 99; verb++) {
+      const target = index3(noun, verb);
+      if (target === 19690720) {
+        return 100 * noun + verb;
+      }
     }
-    return 0;
+  }
+  return 0;
 }
 
 console.log(index3());
